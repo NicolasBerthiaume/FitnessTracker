5a53c9b9e18bdaa6ce22cd1a566e4df05cac6a54
Nicolas Berthiaume <n.berthiaume.language.expert@gmail.com>
2025-05-20 19:48:30 -0400

    Added WeightEntry and CaloriesEntry classes

--- src/main/java/app/FitnessTrackerApp.java
+++ src/main/java/app/FitnessTrackerApp.java
@@ -1,4 +1,21 @@
-﻿package app;
+package app;
 
-public class FitnessTrackerApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class FitnessTrackerApp extends Application {
+    @Override
+    public void start(Stage stage) {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 900, 600);
+        stage.setScene(scene);
+        stage.setTitle("Weight Calories Tracker App");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/model/WeightEntry.java
@@ -0,0 +1,21 @@
+﻿package model;
+
+import java.time.LocalDate;
+
+public class WeightEntry {
+    private final LocalDate date;
+    private final double weight;
+
+    public WeightEntry(LocalDate date, double weight) {
+        this.date = date;
+        this.weight = weight;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
